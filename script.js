03d5dc9b101176a112e8e17ee74eac6f699ca10b
lompdoctor <pleclair@protonmail.com>
2022-04-03 18:14:10 -0500

    Change rounds to prompt, game to round-by-round

--- script.js
+++ script.js
@@ -40,24 +40,23 @@ function playRound (player, cpu) {
     }
 }
 
-function game (num) {
-    let i = 0;
-    while (i < num) {
-        let computerSelection = computerPlay();
-        playRound(playerSelection,computerSelection);
-        i++;
-    }
-    results.textContent=`The final scores are as follows:
+function game () {
+    let computerSelection = computerPlay();
+    playRound(playerSelection,computerSelection);
+    roundsComplete++;
+    results.textContent=`The scores are as follows:
     Player: ${playerScore}
     Computer: ${computerScore}
     Ties: ${ties}`;
-    if (playerScore > computerScore)
-        winner = "The player";
-    else if (playerScore == computerScore)
-        winner = "Nobody"
-    else
-        winner = "The computer";
-    victor.textContent=`${winner} is today's victor.`;
+    if (roundsComplete==rounds) {
+        if (playerScore > computerScore)
+            winner = "The player";
+        else if (playerScore == computerScore)
+            winner = "Nobody"
+        else
+            winner = "The computer";
+        victor.textContent=`${winner} is today's victor.`;
+    }
 }
 
 let playerScore = 0, computerScore = 0, ties = 0;
@@ -65,27 +64,28 @@ let winner;
 
 
 /*let playerSelection = prompt("Choose wisely: Rock, Paper, or Scissors?");*/
-/*let rounds = prompt("How many rounds, friend?");
+let rounds = prompt("How many rounds, friend?");
 
-hardcoded for now*/
-let rounds = 5;
+//hardcoded for now
+//let rounds = 5;
+let roundsComplete = 0;
 const results = document.querySelector('.results');
 const victor = document.querySelector('.victor');
 
 const rock = document.querySelector('#rock');
 rock.addEventListener('click', () => {
     playerSelection = 'rock';
-    game(rounds);
+    game();
 });
 const paper = document.querySelector('#paper');
 paper.addEventListener('click', () => {
     playerSelection = 'paper';
-    game(rounds);
+    game();
 });
 const scissors = document.querySelector('#scissors');
 scissors.addEventListener('click', () => {
     playerSelection = 'scissors';
-    game(rounds);
+    game();
 });
 
 
